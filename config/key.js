1b2387d25abe66519dcb4cb7a1124647101b3685
nidhro <amdadcse13@gmail.com>
2019-09-06 17:22:08 +0600

    an updated commit message

---
+++ config/key.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://nidhro:1234@clusterscifisoft-wpc0d.mongodb.net/mydb'
+}
---
+++ routes/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require('mongoose');
+
+const app = express();
+
+const users = require('./routes/api/users.js');
+const posts = require('./routes/api/posts.js');
+const profile = require('./routes/api/profile.js');
+
+const db = require('./config/key').mongoURI;
+
+mongoose.connect(db)
+    .then(() => console.log('Database connected'))
+    .catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+    res.send("Welcome to Express");
+})
+app.use('/api/users', users);
+app.use('/api/posts', posts);
+app.use('/api/profile', profile);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port,
+    () => console.log(`listening to the port ${port}`))
