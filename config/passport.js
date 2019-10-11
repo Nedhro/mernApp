96b1b23166de7a091052967df05a6ff7d290a5ae
nedhro <nedhror@gmail.com>
2019-10-11 15:32:42 +0600

    issue solved

--- config/passport.js
+++ config/passport.js
@@ -1,27 +1,15 @@
-<<<<<<< HEAD
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const mongoose = require('mongoose');
 const User = mongoose.model('users');
-const keys = require('../config/key');
+const keys = require('../config/keys');
 
 const opts = {};
-=======
-const jwtStrategy = require("passport-jwt").Strategy;
-const ExtractJwt = require("passport-jwt").ExtractJwt;
-const mongoose = require("mongoose");
-const User = mongoose.model("users");
-const keys = require("../config/key");
-
-const opts = {};
-//
->>>>>>> ebccb2827c4d45fae653ca729c02bf2508fea238
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = keys.secretOrKey;
 
 module.exports = passport => {
     passport.use(
-<<<<<<< HEAD
         new JwtStrategy(opts, (jwt_payload, done) => {
             User.findById(jwt_payload.id)
                 .then(user => {
@@ -31,10 +19,6 @@ module.exports = passport => {
                     return done(null, false);
                 })
                 .catch(err => console.log(err));
-=======
-        new jwtStrategy(opts, (jwt_payload, done) => {
-            console.log(jwt_payload);
->>>>>>> ebccb2827c4d45fae653ca729c02bf2508fea238
         })
     );
 };
