62b6ef9d56f8fae1a18201d9c69df1730a624c19
nedhro <amdadcse13@gmail.com>
2021-05-13 19:28:01 +0600

    Version 1.0.1

--- server.js
+++ server.js
@@ -28,6 +28,13 @@ app.use(passport.initialize());
 // Passport Config
 require('./config/passport')(passport);
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+    app.get('*',(req, res)=>{
+        res.sendFile(path.resolve(__dirname, 'client','build', 'index.html'));
+    });
+}
+
 // Use Routes
 app.use('/api/users', users);
 app.use('/api/profile', profile);
